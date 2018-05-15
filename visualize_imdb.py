6dab24b638cf5ad45699ae1290f3d918ce12d8fa
Kentaro KAZAMA <kazehara@outlook.com>
2018-05-15 17:01:38 +0900

    Add inverse word dict

--- visualize_imdb.py
+++ visualize_imdb.py
@@ -5,7 +5,9 @@ from keras.datasets import imdb
 def main():
     word_index = imdb.get_word_index()
 
-    print(word_index)
+    word_index_inv = {v: k for k, v in word_index.items()}
+
+    print(word_index_inv)
 
 
 if __name__ == '__main__':
